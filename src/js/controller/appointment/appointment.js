c8b090a996e8778d9d90d039142065e412561b0e
TALAT <medasmac1@TalatMacPro.local>
2019-04-30 08:49:19 +0400

    update on appointment module

--- src/js/controller/appointment/appointment.js
+++ src/js/controller/appointment/appointment.js
@@ -1,26 +1,17 @@
 angular
     .module('crmApp')
-    .controller('appointmentCtrl', ['$scope','$stateParams' ,'dataFactory', function($scope, $stateParams, dataFactory){
+    .controller('appointmentCtrl', ['$scope', function($scope){
         $scope.data = {}
         $scope.options = {}
         $scope.handlers = {}
-        $scope.calendarView = 'month';
-        $scope.viewDate = new Date();
-        $scope.events = [
-            {
-                title: 'My event title', // The title of the event
-                type: 'info',
-                startsAt: new Date(),
-                endsAt: new Date(2019, 4, 18, 15),
-                editable: false,
-                deletable: false,
-                incrementsBadgeTotal: true
-            }
-           ];
-        $scope.calendarTitle = "Calendar";
+        
         init();
 
         function init() {
-            console.log("pageload");
+            console.log("pageload appointment");
         }
+        
+        $scope.$on('onMonthClick', function(e,d){
+            console.log(d);
+        })
     }]); 
--- src/js/directives/calendar/angular-calender.js
+++ src/js/directives/calendar/angular-calender.js
@@ -31,12 +31,15 @@ angular.module('crmApp')
 
             <!-- month -->
             <div class="month-container border-mattGray-top border-mattGray-right" ng-show="options.calendarView == 'month'">
-                <div class="calendar-header border-mattGray-bottom border-mattGray-right align-text-bottom" ng-repeat="day in options.arrWeekDays">
+                <div 
+                    class="calendar-header border-mattGray-bottom border-mattGray-right align-text-bottom" 
+                    ng-repeat="day in options.arrWeekDays">
                     <p class="text-right pr-1 align-text-bottom">{{day}}</p>
                 </div>
                 <div 
                     class="calendar-item border-mattGray-left border-mattGray-bottom"
                     ng-class="{ 'calendar-item-active': mDay.todayflag }"
+                    ng-click="monthDateClick(mDay)"
                     ng-repeat="mDay in options.monthDays track by $index">
                     <p class="text-right pr-1">{{mDay.day}}</p>
                 </div>
@@ -45,11 +48,19 @@ angular.module('crmApp')
             <!--week-->
             <div ng-show="options.calendarView == 'week'">
                 <div class="week-container border-lightGray-top border-lightGray-right">
-                    <div class="calendar-header border-lightGray-bottom border-lightGray-left"><p class="text-right pr-1 align-text-bottom"></p></div>
-                    <div class="calendar-header border-lightGray-bottom border-lightGray-right align-text-bottom" ng-repeat="day in options.arrWeekDates">
+                    <div 
+                        class="calendar-header border-lightGray-bottom border-lightGray-left">
+                        <p class="text-right pr-1 align-text-bottom"></p>
+                    </div>
+                    <div 
+                        class="calendar-header border-lightGray-bottom border-lightGray-right align-text-bottom" 
+                        ng-repeat="day in options.arrWeekDates">
                         <p class="text-center pr-1 align-text-bottom">{{day.day}}</p>
                     </div>
-                    <div ng-click="timeClick(time)" class="calendar-item border-lightGray-left border-lightGray-bottom" ng-repeat="time in options.times track by $index">
+                    <div 
+                        ng-click="timeClick(time)" 
+                        class="calendar-item border-lightGray-left border-lightGray-bottom" 
+                        ng-repeat="time in options.times track by $index">
                         <p class="text-right pr-1" ng-if="!time.hide">{{time}}</p>
                     </div>
                 </div>
@@ -75,49 +86,49 @@ angular.module('crmApp')
             controller: function ($scope) {
                 $scope.options = {};
                 let dateObject = moment().toObject(), monthIndex = 0, weekIndex = 0, yearIndex = 0, dayIndex = 0, addIndex = 0;
-                
+
                 init();
                 function init(params) {
                     checkCalendarView('month');
                 }
 
-                function setMonthYear(date){
+                function setMonthYear(date) {
                     $scope.options.currentMonth = moment(date).format("MMMM");
                     $scope.options.currentYear = moment(date).format("YYYY");
                 }
 
                 function weekInit() {
-                    if(!monthIndex){
-                        $scope.options.arrWeekDates = weekDates(moment().add(weekIndex,"days"));
-                        setMonthYear(moment().add(weekIndex,"days"));
+                    if (!monthIndex) {
+                        $scope.options.arrWeekDates = weekDates(moment().add(weekIndex, "days"));
+                        setMonthYear(moment().add(weekIndex, "days"));
                     }
                     else {
-                        $scope.options.arrWeekDates = weekDates(moment().add(monthIndex,"M").startOf('month'));
-                        setMonthYear(moment().add(monthIndex,"M").startOf('month'));
+                        $scope.options.arrWeekDates = weekDates(moment().add(monthIndex, "M").startOf('month'));
+                        setMonthYear(moment().add(monthIndex, "M").startOf('month'));
                     }
                     $scope.options.calendarView = 'week';
                     $scope.options.times = timeOneDayOfWeek();
-                    
+
                 }
 
                 function monthInit() {
-                    $scope.options.monthDays = getDaysArrayByMonth(moment().add(addIndex,"M"))
+                    $scope.options.monthDays = getDaysArrayByMonth(moment().add(addIndex, "M"))
                     $scope.options.arrWeekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurstday", "Friday", "Saturday"];
                     $scope.options.calendarView = 'month';
-                    
-                    dateObject = moment().add(addIndex,"M").toObject();
-                    setMonthYear(moment().add(addIndex,"M"));
-                    if(monthIndex && $scope.options.calendarView == 'month')
-                        dateObject.week = parseInt(moment().add(addIndex,"M").startOf('month').format("W"));
+
+                    dateObject = moment().add(addIndex, "M").toObject();
+                    setMonthYear(moment().add(addIndex, "M"));
+                    if (monthIndex && $scope.options.calendarView == 'month')
+                        dateObject.week = parseInt(moment().add(addIndex, "M").startOf('month').format("W"));
                     else
-                        dateObject.week = parseInt(moment().add(addIndex,"M").format("W"));
+                        dateObject.week = parseInt(moment().add(addIndex, "M").format("W"));
 
                 }
 
                 function dayInit() {
-                    
-                    $scope.options.currentDate = moment().add(dayIndex,"days").format("ddd DD");
-                    $scope.options.currentMomentDate = moment().add(dayIndex,"days");
+
+                    $scope.options.currentDate = moment().add(dayIndex, "days").format("ddd DD");
+                    $scope.options.currentMomentDate = moment().add(dayIndex, "days");
                     $scope.options.times = timeCurrentDay();
                     $scope.options.calendarView = 'day';
                     setMonthYear();
@@ -149,16 +160,20 @@ angular.module('crmApp')
                     alert(JSON.stringify(time));
                 }
 
+                $scope.monthDateClick = function (date) {
+                    $scope.$emit('onMonthClick', {dateObj:date})
+                }
+
                 /**
                  * Next
                  */
-                $scope.next = function(){
+                $scope.next = function () {
                     addIndex++;
                     if ($scope.options.calendarView == 'month') {
                         monthIndex++;
                         monthInit();
                     }
-                    else if($scope.options.calendarView == 'week') {
+                    else if ($scope.options.calendarView == 'week') {
                         weekIndex += 7;
                         weekInit();
                     }
@@ -171,13 +186,13 @@ angular.module('crmApp')
                 /**
                  * Prev
                  */
-                $scope.prev = function(){
+                $scope.prev = function () {
                     addIndex--;
                     if ($scope.options.calendarView == 'month') {
                         monthIndex--;
                         monthInit();
                     }
-                    else if($scope.options.calendarView == 'week') {
+                    else if ($scope.options.calendarView == 'week') {
                         weekIndex -= 7;
                         weekInit();
                     }
@@ -190,7 +205,7 @@ angular.module('crmApp')
                 /**
                  * Today
                  */
-                $scope.today = function(){
+                $scope.today = function () {
                     addIndex = 0;
                     monthIndex = 0;
                     weekIndex = 0;
@@ -214,7 +229,7 @@ angular.module('crmApp')
                         day = day.clone().add(1, 'd');
                     }
 
-                    function addObj(){
+                    function addObj() {
                         return {
                             day: day.format("ddd DD"),
                             momentDate: day
@@ -226,19 +241,18 @@ angular.module('crmApp')
                 /**
                  * Get Time Array for Current Date
                  */
-
                 function timeCurrentDay() {
                     const hoursPerDay = 24;
                     let time = [];
                     let formattedTime;
                     console.log(dayIndex);
                     for (i = 0; i < hoursPerDay + 1; i++) { //fill in all of the hours
-                        (!dayIndex) ? formattedTime = (moment().subtract(i, "hours")).format("hA") : formattedTime = moment().startOf('day').add(i,"hours").format("hA")  //give the time in format X AM/PM
-                        
+                        (!dayIndex) ? formattedTime = (moment().subtract(i, "hours")).format("hA") : formattedTime = moment().startOf('day').add(i, "hours").format("hA")  //give the time in format X AM/PM
+
                         fillTimes(); // fill blank time
                         time.push(formattedTime);  //add to beginning of array
                     } //do this for all 24 hours
-                    
+
                     return time.reverse();
 
                     function fillTimes() {
@@ -258,7 +272,6 @@ angular.module('crmApp')
                 /**
                  * get Time Array of Day for week
                  */
-
                 function timeOneDayOfWeek() {
                     const hoursPerDay = 24;
                     let time = [];
@@ -268,7 +281,7 @@ angular.module('crmApp')
                         fillTimes(); // fill blank time
                         time.push(formattedTime);  //add to beginning of array
                     } //do this for all 24 hours
-                    
+
                     return time.reverse();
 
                     function fillTimes() {
@@ -287,7 +300,7 @@ angular.module('crmApp')
                 /**
                  * Check if the date id today and return boolean
                  */
-                function checkToday(date){
+                function checkToday(date) {
                     return (Math.floor(moment.duration(moment().diff(date)).asDays()) == 0);
                 }
 
@@ -308,11 +321,11 @@ angular.module('crmApp')
                     function addDays(noOfDays, startDate) {
                         let index = 1;
                         // fisrt day of month with month name e.g May 1
-                        addObj(moment(startDate).add(index,"days").format("MMM D"),startDate);
+                        addObj(moment(startDate).add(index, "days").format("MMM D"), startDate);
                         index++;
                         //loop to fill the array
                         while ((7 - noOfDays) != index) {
-                            addObj(moment(startDate).add(index,"days").format("D"),startDate);
+                            addObj(moment(startDate).add(index, "days").format("D"), startDate);
                             index++;
                         }
                     }
@@ -320,7 +333,7 @@ angular.module('crmApp')
                     function removeDays(noOfDays, startDate) {
                         noOfDays = noOfDays * -1;
                         while (noOfDays) {
-                            addObj(moment(startDate).add(noOfDays,"days").format("D"),startDate);
+                            addObj(moment(startDate).add(noOfDays, "days").format("D"), startDate);
                             noOfDays++;
                         }
                     }
@@ -328,26 +341,27 @@ angular.module('crmApp')
                     //Month days
                     function monthDays() {
                         let index = 0;
-                        _date.startOf('month').add(index,"d").format("D");
-                        addObj(moment(_date).startOf('month').add(index,"d").format("MMM D"), _date);
+                        _date.startOf('month').add(index, "d").format("D");
+                        addObj(moment(_date).startOf('month').add(index, "d").format("MMM D"), _date);
 
                         //increament of first day
-                        index++; 
+                        index++;
 
                         while (daysInMonth != index) {
-                            const d = _date.startOf('month').add(index,"d").format("D");
+                            const d = _date.startOf('month').add(index, "d").format("D");
                             addObj(d, _date);
                             index++;
                         }
 
-                        
+
                     }
 
                     //add date obj
-                    function addObj(d, momentDate){  
+                    function addObj(d, momentDate) {
                         arrDays.push({
                             day: d,
-                            todayflag: checkToday(momentDate)
+                            todayflag: checkToday(momentDate),
+                            momentDate
                         });
                     }
                     return arrDays;
